3b4ff282d496a9ae20c13a2927a0d76f7af4728d
Sunny <sunny.ajimal@nology.io>
2020-02-18 15:04:14 +0000

    moving spaceship

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-//to make the ship move left and right 
+//to make the ship move left and right
+
+const heroElement = document.getElementById("hero");
+
+const hero = {
+    left: 550,
+    top: 450
+}
+
 document.onkeydown = (e) => {
+    e.preventDefault();
     console.log(e);
     if (e.keyCode === 37){
         console.log("LEFT")
-    }
-
-    console.log(e);
-    if (e.keyCode === 39){
+        hero.left = hero.left - 10;
+        heroElement.style.left = hero.left + "px";
+    } else if (e.keyCode === 39){
         console.log("RIGHT")
+        hero.left = hero.left + 10;
+        heroElement.style.left = hero.left + "px";
     }
-
-}
+    
+};
